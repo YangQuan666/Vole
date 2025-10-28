4032a40f900196e5f5d295e7ebfd2ec2876699d6
Frank <quark.yeung@icloud.com>
2025-10-28 23:34:00 +0800

    Create NodeDetailView.swift

---
+++ Vole/Views/Node/NodeDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  NodeDetailView.swift
+//  Vole
+//
+//  Created by 杨权 on 10/28/25.
+//
+
+import SwiftUI
+
+struct NodeDetailView: View {
+    
+    let nodeName: String
+    
+    var body: some View {
+        Text("Hello, \(nodeName)!")
+    }
+}
+
+#Preview {
+//    NodeDetailView()
+}
