864b627c9e8fb7ab9f62ada32328388300f4bb91
Frank <quark.yeung@icloud.com>
2025-11-22 16:59:30 +0800

    init search

--- Vole/Views/Search/SearchView.swift
+++ Vole/Views/Search/SearchView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    @EnvironmentObject var navManager: NavigationManager
+
     var body: some View {
-        Text("Hello, Search!")
+        NavigationStack(path: $navManager.searchPath) {
+            List {
+                if searchText.isEmpty {
+                    Text("请输入关键词进行搜索")
+                        .foregroundColor(.secondary)
+                } else {
+                    // 这里填搜索结果
+                    Text("搜索结果: \(searchText)")
+                }
+            }
+            .navigationTitle("搜索")
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .automatic)
+            )
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .topicId(let topicId):
+                    DetailView(topicId: topicId, path: $navManager.searchPath)
+                case .nodeName(let nodeName):
+                    NodeDetailView(
+                        nodeName: nodeName,
+                        path: $navManager.nodePath
+                    )
+                case .node(let node):
+                    NodeDetailView(node: node, path: $navManager.searchPath)
+                default: EmptyView()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "person.crop.circle.fill")
+                        .font(.system(size: 28))
+                        .foregroundStyle(.gray)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SearchView()
+    @Previewable var navManager = NavigationManager()
+    SearchView().environmentObject(navManager)
 }
